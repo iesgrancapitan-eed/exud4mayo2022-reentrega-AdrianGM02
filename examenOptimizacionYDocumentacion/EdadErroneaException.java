98bed716e8fc0d2ac5939f5fbb2b3effeab596d6
Adrian Gonzalez <a21gomaad@iesgrancapitan.org>
2022-06-01 19:33:03 +0200

    fix #1

---
+++ examenOptimizacionYDocumentacion/EdadErroneaException.java
@@ -0,0 +1,11 @@
+package examenOptimizacionYDocumentacion;
+
+public class EdadErroneaException extends Exception {
+  
+  
+  //Esto es un comentario para esta clase.
+
+	public EdadErroneaException(String string) {
+		super(string);
+	}
+}
