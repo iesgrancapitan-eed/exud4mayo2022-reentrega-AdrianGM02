d848f0f2fc99f372b97d70e49fc44a03ee9cd5e3
Adrian Gonzalez <a21gomaad@iesgrancapitan.org>
2022-06-01 19:39:50 +0200

    refactor: mensaje de la excepcion

--- src/com/AGM/eed/examen/optimizacionydocumentacion/EdadErroneaException.java
+++ src/com/AGM/eed/examen/optimizacionydocumentacion/EdadErroneaException.java
@@ -5,7 +5,7 @@ public class EdadErroneaException extends Exception {
   
   //Esto es un comentario para esta clase.
 
-	public EdadErroneaException(String string) {
-		super(string);
+	public EdadErroneaException(String message) {
+		super(message);
 	}
 }
