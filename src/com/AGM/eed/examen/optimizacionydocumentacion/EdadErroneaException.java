faf2f530ebc71d46e56779c377367fbe18427898
Adrian Gonzalez <a21gomaad@iesgrancapitan.org>
2022-06-01 19:38:18 +0200

    refactor: nombre del paquete

---
+++ src/com/AGM/eed/examen/optimizacionydocumentacion/EdadErroneaException.java
@@ -0,0 +1,11 @@
+package com.AGM.eed.examen.optimizacionydocumentacion;
+
+public class EdadErroneaException extends Exception {
+  
+  
+  //Esto es un comentario para esta clase.
+
+	public EdadErroneaException(String string) {
+		super(string);
+	}
+}
