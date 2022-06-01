b87cf7a813067b0f4b7bb8d3f3170dc9682c90a4
Adrian Gonzalez <a21gomaad@iesgrancapitan.org>
2022-06-01 19:43:59 +0200

    refactor: mascota

---
+++ src/com/AGM/eed/examen/optimizacionydocumentacion/Mascota.java
@@ -0,0 +1,17 @@
+package com.AGM.eed.examen.optimizacionydocumentacion;
+
+
+
+public class Mascota {
+
+  protected String name;
+
+  public Mascota() {
+    super();
+  }
+
+  public String getName() {
+  	return name;
+  }
+
+}
